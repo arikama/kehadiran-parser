09b88a2837cb9586c0bbdcce96ae43e47df3e8b9
Hanif Ariffin <hanif.ariffin.4326@gmail.com>
2023-06-04 10:50:21 +0800

    Update the way that the CLI accepts input
    1. Uses cmdline argument instead of config.yaml (maybe add option to use config.yaml in the future?)
    2. Change main to return Result instead of using error code.
    3. Validate that the given paths are indeed directories and not files.
    
    Signed-off-by: Hanif Ariffin <hanif.ariffin.4326@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ use std::collections::HashSet;
 use std::fs;
 use std::fs::File;
 use std::io::prelude::*;
+use std::path::Path;
 
 #[derive(Serialize, Deserialize)]
 struct ParliamentMeeting {
@@ -35,41 +36,38 @@ lazy_static! {
     static ref NAME_REGEX: Regex = Regex::new(r"[A-Z()][\w,’'()@/\.\- ]+").unwrap();
 }
 
-pub fn run(pdf_dir: String, out_dir: String) -> i32 {
-    println!("Starting: pdf_dir={}, out_dir={}", pdf_dir, out_dir);
-    if let Ok(entries) = fs::read_dir(pdf_dir) {
-        for entry in entries {
-            if let Ok(entry) = entry {
-                let file_path = entry.path();
-                if file_path.is_dir() {
-                    continue;
-                }
-                if let Some(ext) = file_path.extension() {
-                    if ext != "pdf" {
-                        continue;
-                    }
-                } else {
-                    continue;
-                }
-                println!("Processing: pdf_file={}", file_path.display());
-                let bytes = std::fs::read(file_path.display().to_string()).unwrap();
-                let content = pdf_extract::extract_text_from_mem(&bytes).unwrap();
-                let json_file_path = file_path
-                    .display()
-                    .to_string()
-                    .trim_end_matches(".pdf")
-                    .clone()
-                    .to_owned()
-                    + ".json";
-                if let Ok(mut json_file) = File::create(json_file_path) {
-                    let parsed = parse(content);
-                    let json_string = serde_json::to_string_pretty(&parsed).unwrap();
-                    if let Ok(()) = json_file.write_all(json_string.as_bytes()) {}
-                }
+pub fn run(pdf_dir: &Path, _: &Path) -> Result<(), Box<dyn std::error::Error>> {
+    let entries = fs::read_dir(pdf_dir)?;
+    for entry in entries {
+        let entry = entry?;
+        let file_path = entry.path();
+        if file_path.is_dir() {
+            continue;
+        }
+        if let Some(ext) = file_path.extension() {
+            if ext != "pdf" {
+                continue;
             }
+        } else {
+            continue;
+        }
+        println!("Processing: pdf_file={}", file_path.display());
+        let bytes = std::fs::read(file_path.display().to_string())?;
+        let content = pdf_extract::extract_text_from_mem(&bytes)?;
+        let json_file_path = file_path
+            .display()
+            .to_string()
+            .trim_end_matches(".pdf")
+            .clone()
+            .to_owned()
+            + ".json";
+        if let Ok(mut json_file) = File::create(json_file_path) {
+            let parsed = parse(content);
+            let json_string = serde_json::to_string_pretty(&parsed)?;
+            json_file.write_all(json_string.as_bytes())?;
         }
     }
-    0
+    Ok(())
 }
 
 fn parse(content: String) -> ParliamentMeeting {
